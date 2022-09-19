826e564b3d72c4e8c92a5ac2a0014f02291c4e3f
Kuldeep Ahlawat <84150035+imkuldeepahlawat@users.noreply.github.com>
2022-09-19 17:42:03 +0530

    Add files via upload

---
+++ Lecture 1/l1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+	/*cout<< "Namaste Dunia :-) " <<endl;*/
+	/*int a = 2.0/5;
+	cout << a << endl;*/
+	/*int a = 2;
+	int b = 3 ;
+	bool c = (a==b);
+	cout<< c << endl;
+
+	bool d = (a<b);
+	cout<< d << endl;
+
+	bool e = (a>b);
+	cout<< e << endl;
+
+	bool f = (a<=b);
+	cout<< f << endl;
+
+	bool g = (a>=b);
+	cout<< g << endl;
+
+	bool h = (a!=b);
+	cout<< h << endl;*/
+	/*int a = 21;
+	cout << !a << endl;*/
+	cout << " Parimeter of a rectangle = 2(L + W)";
+	int l = 5, b = 7;
+	int p = 2*(l+b);
+	cout<<p<<endl;
+	cout << "Area is " << 5*7 << endl;
+	cout << "Perimeter is " << 2*(5+7) << endl;
+}
