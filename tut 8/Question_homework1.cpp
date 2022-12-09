ed892881f065f5f7b073e2ec7bf8e3893b4fa495
imkuldeepahlawat <imkuldeepahlawat@hotmail.com>
2022-12-09 13:52:43 +0530

    tut 8

---
+++ tut 8/Question_homework1.cpp
@@ -0,0 +1,13 @@
+// A.P
+#include<bits/stdc++.h>
+using namespace std;
+
+int apFunc(int n){
+    int ans = 3 * n + 7;
+    cout<<"The answer is :- ";
+    return ans;
+}
+int main(){
+    int a = 1;
+    cout<<apFunc(a);
+}
---
+++ tut 8/coUn.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void countint(int n){
+    for(int i = 0;i<=n;i++){
+        cout<<i<<endl;
+    }
+    return ;
+}
+int main(){
+    int a = 15;
+    countint(a);
+}
